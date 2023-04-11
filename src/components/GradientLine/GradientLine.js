d843f647dfcc25b3d7bf044478d2bf436af3f3e1
Allex372 <alexalex372.aa@gmail.com>
2023-04-11 17:08:52 +0300

    add basic structure

---
+++ src/components/GradientLine/GradientLine.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+import * as styles from './gradientline.module.css';
+
+const GradientLine = () => {
+    return (
+        <div className={styles.gradientLine} />
+    )
+
+}
+
+export default GradientLine;
