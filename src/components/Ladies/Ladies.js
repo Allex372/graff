dc4dc44019c047fe4b3f77c83b121e3296ae0e9a
Allex372 <alexalex372.aa@gmail.com>
2023-04-14 18:39:56 +0300

    add new modal

--- src/components/Ladies/Ladies.js
+++ src/components/Ladies/Ladies.js
@@ -1,75 +1,55 @@
 import * as React from "react"
+import { useState } from 'react';
 
-import img from '../../images/models/girl.webp';
+import ModelsDialog from "../ModelsDialog/ModelsDialog";
+import { AllModelsArray } from "../../consts/allModelsArray";
 import * as styles from './ladies.module.css';
 
 const Ladies = () => {
+    const [openDialog, setOpenDialog] = useState(false);
+    const [arr, setArr] = useState([]);
+
+    const handleOpenDialog = (imagesArray) => {
+        setArr([]);
+        if (imagesArray) {
+            setArr(imagesArray);
+        }
+        setOpenDialog(true);
+    }
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false)
+    }
+
     return (
         <div className={styles.wrapper}>
             <p className={styles.title}>Our Ladies</p>
             <div className={styles.flexWrapper}>
                 <div className={styles.cardWrapper}>
-
-                    <div className={styles.card}>
-                        <div className={styles.content}>
-                            <div className={styles.imgBx}>
-                                <img src={img} alt='girl' />
+                    {AllModelsArray.map((el) => (
+                        <div className={styles.card} key={el.id}>
+                            <div className={styles.content}>
+                                <div className={styles.imgBx}>
+                                    <img src={el.image} alt='girl' onClick={() => handleOpenDialog(el.allImages)} />
+                                </div>
                             </div>
+
+                            <ul className={styles.sci}>
+                                <li>
+                                    <p className={styles.ladyText}>Params</p>
+                                </li>
+                                <li>
+                                    <p className={styles.ladyText}>Params</p>
+                                </li>
+                                <li>
+                                    <p className={styles.ladyText}>Params</p>
+                                </li>
+                            </ul>
                         </div>
-
-                        <ul className={styles.sci}>
-                            <li>
-                                <p className={styles.ladyText}>Params</p>
-                            </li>
-                            <li>
-                                <p className={styles.ladyText}>Params</p>
-                            </li>
-                            <li>
-                                <p className={styles.ladyText}>Params</p>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className={styles.card}>
-                        <div className={styles.content}>
-                            <div className={styles.imgBx}>
-                                <img src={img} alt='girl' />
-                            </div>
-                        </div>
-                        <ul className={styles.sci}>
-                            <li>
-                                <p className={styles.ladyText}>Params</p>
-                            </li>
-                            <li>
-                                <p className={styles.ladyText}>Params</p>
-                            </li>
-                            <li>
-                                <p className={styles.ladyText}>Params</p>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className={styles.card}>
-                        <div className={styles.content}>
-                            <div className={styles.imgBx}>
-                                <img src={img} alt='girl' />
-                            </div>
-                        </div>
-                        <ul className={styles.sci}>
-                            <li>
-                                <p className={styles.ladyText}>Params</p>
-                            </li>
-                            <li>
-                                <p className={styles.ladyText}>Params</p>
-                            </li>
-                            <li>
-                                <p className={styles.ladyText}>Params</p>
-                            </li>
-                        </ul>
-                    </div>
-
+                    ))}
                 </div>
             </div>
+            <ModelsDialog isOpenDialog={openDialog} handleClose={() => handleCloseDialog()} imagesArray={arr.length >= 1 && arr} />
         </div>
     )
 }
---
+++ src/consts/allModelsArray.js
@@ -0,0 +1,32 @@
+import Kira from '../images/models/Kira/Kira.jpg';
+import Yana from '../images/models/Yana/Yana.jpg';
+import Girl from '../images/models/girl.webp';
+
+import Yana1 from '../images/models/Yana/Yana1.jpg';
+import Yana2 from '../images/models/Yana/Yana2.jpg';
+
+import Kira1 from '../images/models/Kira/Kira1.png';
+import Kira2 from '../images/models/Kira/Kira2.jpg';
+import Kira3 from '../images/models/Kira/Kira3.jpg';
+import Kira4 from '../images/models/Kira/Kira4.png';
+
+
+export const AllModelsArray = [
+    {
+        id: 1,
+        image: Kira,
+        name: 'Кіра',
+        allImages: [Kira, Kira1, Kira2, Kira3, Kira4]
+    },
+    {
+        id: 2,
+        image: Yana,
+        name: 'Yana',
+        allImages: [Yana, Yana1, Yana2]
+    },
+    {
+        id: 3,
+        image: Girl,
+        name: 'Any'
+    }
+]
