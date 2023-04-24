3c7ddb248fd12cf034e28d557de8c03057348719
Allex372 <alexalex372.aa@gmail.com>
2023-04-24 11:49:36 +0300

    fix styles

--- src/components/logoScreen/LogoScreen.js
+++ src/components/logoScreen/LogoScreen.js
@@ -6,7 +6,7 @@ import GradientLine from "../GradientLine/GradientLine";
 import logo from '../../images/logo.png';
 import menuIcon from '../../images/menu-icon.png';
 import closeIcon from '../../images/close-icon.png';
-
+// 0986374614
 import * as styles from './logo.module.css';
 
 const LogoScreen = () => {
@@ -26,31 +26,34 @@ const LogoScreen = () => {
         <div className={styles.wrapper}>
             <div className={styles.iconContentWrapper}>
                 <div className={styles.imageWrapper}>
-                    <div className={styles.logoWrapper} style={{ zIndex: isOpen && '2' }}>
-                        <img src={logo} alt='logo' />
-                    </div>
+                    {
+                        !isOpen &&
+                        <div className={styles.logoWrapper} style={{ zIndex: isOpen && '2' }}>
+                            <img src={logo} alt='logo' />
+                        </div>
+                    }
                     <div className={styles.menuIcon} style={{ zIndex: isOpen && '2' }} onClick={() => handleOpenMobileMenu()}>
                         {isOpen ? <img src={closeIcon} alt="close" /> : <img src={menuIcon} alt='open' />}
                     </div>
                 </div>
                 <div className={styles.contactWrapper}>
                     <div className={styles.contactsInfo}>
-                        <a href="tel:+1234567890">
+                        <a href="tel:+1234567890" target='_blank' rel="noreferrer">
                             <i class="fas fa-phone fa-lg"></i>
                             +1 (234) 567-890
                         </a>
 
-                        <a href="tg://resolve?domain=telegram_username">
+                        <a href="tg://resolve?domain=telegram_username" target='_blank' rel="noreferrer">
                             <i class="fab fa-telegram fa-lg"></i>
                             Telegram
                         </a>
 
-                        <a href="https://wa.me/1234567890">
+                        <a href="https://wa.me/1234567890" target='_blank' rel="noreferrer">
                             <i class="fab fa-whatsapp fa-lg"></i>
                             WhatsApp
                         </a>
 
-                        <a href="viber://pa?chatURI=viber_username">
+                        <a href="viber://pa?chatURI=viber_username" target='_blank' rel="noreferrer">
                             <i class="fab fa-viber fa-lg"></i>
                             Viber
                         </a>
@@ -64,7 +67,8 @@ const LogoScreen = () => {
                 <Header />
             </div>
 
-            {isOpen &&
+            {
+                isOpen &&
                 <div className={`${styles.mobileMenu} ${isOpen ? styles.open : ""}`}>
                     <div className={styles.mobileHeaderWrapper}>
                         <Header />
@@ -95,7 +99,7 @@ const LogoScreen = () => {
                     </div>
                 </div>
             }
-        </div>
+        </div >
     )
 
 }
