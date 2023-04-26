44107db91fd3f7346a0396be1a731d6a08074b4a
Allex372 <alexalex372.aa@gmail.com>
2023-04-26 17:11:21 +0300

    fix

--- src/components/InterierGalery/InterierGalery.js
+++ src/components/InterierGalery/InterierGalery.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 
 import SwiperCarousel from "../Swiper/Swiper";
+import { useLanguage } from "../../context/languageContext";
 
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
@@ -11,9 +12,10 @@ import * as styles from './InterierGarely.module.css';
 
 
 const InterierGalery = () => {
+    const { t } = useLanguage();
     return (
         <div className={styles.wrapper} id="galery">
-            <p className={styles.title}>Galery Block</p>
+            <p className={styles.title}>{t('interior')}</p>
             <div className={styles.container}>
                 <SwiperCarousel />
             </div >
